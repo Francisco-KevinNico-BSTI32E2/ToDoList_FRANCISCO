1dae1de423f3deec61afb19256a8679a44cff18e
Francisco_KevinNico_BSIT32E2 <161100540+Francisco-KevinNico-BSTI32E2@users.noreply.github.com>
2024-05-15 22:34:28 +0800

    Done

--- ToDoList_FRANCISCO/Controllers/ToDoController.cs
+++ ToDoList_FRANCISCO/Controllers/ToDoController.cs
@@ -67,7 +67,6 @@ namespace ToDoList_FRANCISCO.Controllers
         }
 
         [HttpPost]
-        [ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public IActionResult DeleteConfirmed(int id)
         {
--- ToDoList_FRANCISCO/Program.cs
+++ ToDoList_FRANCISCO/Program.cs
@@ -28,6 +28,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=ToDo}/{action=Index}/{id?}");
+    pattern: "{controller=ToDo}/{action=Index}/{id:int?}");
 
 app.Run();
